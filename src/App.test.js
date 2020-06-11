68095dda305a40a59e9e36dc7658fadc0e285970
ClydeFrog04 <egan.randal@gmail.com>
2020-06-10 19:30:29 -0600

    Project setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
 
 test("renders App without crashing", () => {
   render(<App />);
+});
+
+test("Renders Contact Form without crashing", () =>{
+    const {getByTestId} = render(<ContactForm/>);
+    getByTestId(/submitButton/i);
 });
